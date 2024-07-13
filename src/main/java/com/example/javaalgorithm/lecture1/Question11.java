cf61047d40ed39263d59fb596cf508eb542ec49c
Lee <zxzc9712@naver.com>
2024-07-13 10:58:52 +0900

    1강 11번 문자열 압축

---
+++ src/main/java/com/example/javaalgorithm/lecture1/Question11.java
@@ -0,0 +1,62 @@
+package com.example.javaalgorithm.lecture1;
+import java.util.*;
+
+/**
+ * 11. 문자열 압축
+ * 설명
+ *
+ * 알파벳 대문자로 이루어진 문자열을 입력받아 같은 문자가 연속으로 반복되는 경우 반복되는
+ *
+ * 문자 바로 오른쪽에 반복 횟수를 표기하는 방법으로 문자열을 압축하는 프로그램을 작성하시오.
+ *
+ * 단 반복횟수가 1인 경우 생략합니다.
+ *
+ *
+ * 입력
+ * 첫 줄에 문자열이 주어진다. 문자열의 길이는 100을 넘지 않는다.
+ *
+ *
+ * 출력
+ * 첫 줄에 압축된 문자열을 출력한다.
+ *
+ *
+ * 예시 입력 1
+ *
+ * KKHSSSSSSSE
+ * 예시 출력 1
+ *
+ * K2HS7E
+ */
+public class Question11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char prev = str.charAt(0);
+        StringBuilder answer = new StringBuilder();
+        int cnt = 1;
+
+        for (int i = 1; i < str.length(); i++) {
+            if (prev == str.charAt(i)) {
+                //이전문자와 같으면 cnt 증가
+                cnt++;
+            } else {
+                answer.append(prev);
+                if (cnt > 1) {
+                    answer.append(cnt);
+                }
+                cnt = 1;
+                prev = str.charAt(i);
+            }
+
+            if (i == str.length()-1) {
+                answer.append(prev);
+                if (cnt > 1) {
+                    answer.append(cnt);
+                }
+            }
+        }
+
+        System.out.println(answer.toString());
+        sc.close();
+    }
+}
